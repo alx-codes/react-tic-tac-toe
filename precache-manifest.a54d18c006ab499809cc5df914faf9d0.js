ffacc5b23b43f19283167595e9270f9594dfb9fb
Alejandro Figueroa <aleckzf@gmail.com>
2020-01-15 15:05:58 -0600

    Updates

---
+++ precache-manifest.a54d18c006ab499809cc5df914faf9d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff561509dc8e9171a48dc164d5a40b0d",
+    "url": "/alx-codes/react-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "f2cf04e0e1c08c421ba8",
+    "url": "/alx-codes/react-tic-tac-toe/static/css/main.80220f6d.chunk.css"
+  },
+  {
+    "revision": "e10ba2bfb29b605f3061",
+    "url": "/alx-codes/react-tic-tac-toe/static/js/2.c4022d2a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/alx-codes/react-tic-tac-toe/static/js/2.c4022d2a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f2cf04e0e1c08c421ba8",
+    "url": "/alx-codes/react-tic-tac-toe/static/js/main.bc6bb675.chunk.js"
+  },
+  {
+    "revision": "f9c74b1124f5965d324e",
+    "url": "/alx-codes/react-tic-tac-toe/static/js/runtime-main.2ffce219.js"
+  }
+]);
